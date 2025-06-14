0daa6e31e6f985c41406b178efc7a7ed990c4834
KateSt <kateystetsenko@gmail.com>
2025-06-14 13:54:34 +0300

    add toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextPassword;
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +30,19 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        addListenerOnButton();
     }
 
+    public void addListenerOnButton(){
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,editTextPassword.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
     public void onButtonClick(View view) {
         EditText num1 = (EditText) findViewById(R.id.Num1);
         EditText num2 = (EditText) findViewById(R.id.Num2);
